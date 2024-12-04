3ad6b75a620c6706db394e3bbbd3327add2794c6
Andrey Maltsev <andrei@maltsev.me>
2024-12-05 00:13:38 +0300

    first success version

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ import pytest
 @allure.title("Verify API returns status 200 and response 'ok'")
 def test_api_response():
     # Define the API endpoint
-    url = "http://example.com/api"  # Replace with your actual API endpoint
+    url = "https://dog.ceo/api/breeds/image/random"  # Replace with your actual API endpoint
 
     # Send a GET request with the parameter
-    response = requests.get(url, params={'value1': 'testvalue'})
+    response = requests.get(url)
+
+    print(response)
 
     # Check if the status code is 200
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
 
     # Check if the returned value is 'ok'
     json_response = response.json()  # Assuming the response is in JSON format
-    assert json_response.get('value') == 'ok', f"Expected 'ok' but got {json_response.get('value')}"
+    assert json_response.get('status') == 'success', f"Expected 'ok' but got {json_response.get('value')}"
 
 test_api_response()
